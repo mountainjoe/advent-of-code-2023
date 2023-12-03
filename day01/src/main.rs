e1809bd2e7599bfe5cc72b794c6989dbd607856a
Joseph Olivier <joseph.olivier@gmail.com>
2023-12-03 15:44:26 -0800

    Add (ugly,correct) solution

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -21,12 +21,18 @@ fn main() {
     let file = file.unwrap();
     let reader = BufReader::new(file);
 
+    let mut sum = 0;
     for line in reader.lines() {
         let line = line.unwrap();
         println!("input_line: {}", line);
         let twochars = line_to_two_chars(&line);
         println!("twochars: {:?}", twochars);
+        let num = two_chars_to_number(&twochars);
+        println!("value: {:?}", num);
+        sum += num;
     }
+
+    println!("sum: {}", sum);
 }
 
 // convert a text line into the two characters of interest
@@ -38,4 +44,9 @@ fn line_to_two_chars(line: &str) -> (char, char) {
         char::from_u32(line.as_bytes()[s_index] as u32).unwrap(),
         char::from_u32(line.as_bytes()[e_index] as u32).unwrap()
     )
+}
+
+// take two characters and turn them into the desired number (concatenate)
+fn two_chars_to_number(two_chars: &(char,char)) -> u32 {
+    10 * two_chars.0.to_digit(10).unwrap() + two_chars.1.to_digit(10).unwrap()
 }
