889d1e46757565aac904291ee555f3468efe60d1
Joseph Olivier <joseph.olivier@gmail.com>
2023-12-03 10:00:21 -0800

    Add extraction of begin and end chars

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -22,6 +22,20 @@ fn main() {
     let reader = BufReader::new(file);
 
     for line in reader.lines() {
-        println!("input_line: {}", line.unwrap());
+        let line = line.unwrap();
+        println!("input_line: {}", line);
+        let twochars = line_to_two_chars(&line);
+        println!("twochars: {:?}", twochars);
     }
+}
+
+// convert a text line into the two characters of interest
+fn line_to_two_chars(line: &str) -> (char, char) {
+    let s_index = line.find(|c: char| c.is_ascii_digit()).unwrap(); // panic if not found; presume good input
+    let e_index = line.rfind(|c: char| c.is_ascii_digit()).unwrap();
+
+    (
+        char::from_u32(line.as_bytes()[s_index] as u32).unwrap(),
+        char::from_u32(line.as_bytes()[e_index] as u32).unwrap()
+    )
 }
