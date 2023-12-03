4b7a94eb85dce91d7bfdecd374467d878745734d
Joseph Olivier <joseph.olivier@gmail.com>
2023-12-03 09:25:11 -0800

    Add code to read file line-by-line

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,3 +1,6 @@
+use std::fs::File;
+use std::io::{BufRead,BufReader};
+
 use clap::Parser;
 
 #[derive(Parser)]
@@ -8,5 +11,17 @@ struct Args {
 
 fn main() {
     let args = Args::parse();
-    println!("input: {:?}", args.puzzle_input);
+
+    let file = File::open(&args.puzzle_input);
+    if let Err(e) = file {
+        eprintln!("Failed to open {:?}: {:?}", &args.puzzle_input, e);
+        return
+    }
+
+    let file = file.unwrap();
+    let reader = BufReader::new(file);
+
+    for line in reader.lines() {
+        println!("input_line: {}", line.unwrap());
+    }
 }
