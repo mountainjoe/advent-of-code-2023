e1418742cd6a54887631371a4ec36a549aa18be6
Joseph Olivier <joseph.olivier@gmail.com>
2023-12-18 19:58:54 -0800

    Add solution to day04 part2

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -1,7 +1,7 @@
 use std::collections::HashSet;
 
 use clap::Parser as _;
-use pest::{Parser, iterators::{Pairs, Pair}};
+use pest::{Parser, iterators::Pairs};
 
 #[derive(clap::Parser)]
 #[command(author,version,about,long_about=None)]
@@ -24,11 +24,25 @@ fn main() {
     //eprintln!("cards: {:?}", cards);
 
     // ok, now we just do scoring
-    let scores: Vec<_> = cards.iter().map(|c| score_card(c)).collect();
-    eprintln!("scores: {:?}", scores);
+    let card_scores: Vec<_> = cards.iter().map(|c| (c.n, score_card(c))).collect();
+    eprintln!("card_scores: {:?}", card_scores);
 
-    let sum: usize = scores.iter().sum();
-    eprintln!("sum: {}", sum);
+    // now we gotta make this into a map... so I can lookup and sum. Actually no... vector lookup should be fine.
+    let mut card_stack: Vec<usize> = Vec::new();
+
+    for cs in card_scores.iter().rev() {
+        //eprintln!("card {} wins {}", cs.0, cs.1);
+        // most recent n scores are on the vector as a stack
+        // now we need to use the score of this card to determine the remainder... damn recursion
+        
+        let sum: usize = card_stack.iter().rev().take(cs.1).sum();
+        card_stack.push(1 + sum);
+    }
+
+    eprintln!("card stack: {:?}", card_stack);
+
+    let sum: usize = card_stack.iter().sum();
+    eprintln!("total = {}", sum); // correct final answer = 14427616
 }
 
 #[derive(Debug)]
@@ -75,11 +89,5 @@ fn card_from_pair(mut p: Pairs<Rule>) -> Card {
 fn score_card(card: &Card) -> usize {
     // first map ours to wins
     let wins = card.ours.iter().filter(|n| card.winners.contains(n)).count();
-    match wins {
-        0 => 0,
-        1 => 1,
-        _ => {
-            2_usize.pow( (wins - 1) as u32)
-        }
-    }
+    wins
 }
